83a90873c394f9e92ca362457aead2d8a6960236
abhiperfect <abhishekprajapati.890e@gmail.com>
2023-09-17 18:43:34 +0530

    Chnage fronted , ready to go with javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+var songs = [
+    { TrackImage:"./images/dua_lipa.jpg", TrackName:"Hotter Than Hell", TrackArtist:"Dua Lipa", TrackAlbum:"Kiss", TrackDuration:"5:45"},
+    { TrackImage:"./images/dua_lipa.jpg", TrackName:"Hotter Than Hell", TrackArtist:"Dua Lipa", TrackAlbum:"Kiss", TrackDuration:"5:45"},
+    { TrackImage:"./images/dua_lipa.jpg", TrackName:"Hotter Than Hell", TrackArtist:"Dua Lipa", TrackAlbum:"Kiss", TrackDuration:"5:45"},
+    { TrackImage:"./images/dua_lipa.jpg", TrackName:"Hotter Than Hell", TrackArtist:"Dua Lipa", TrackAlbum:"Kiss", TrackDuration:"5:45"},
+    { TrackImage:"./images/dua_lipa.jpg", TrackName:"Hotter Than Hell", TrackArtist:"Dua Lipa", TrackAlbum:"Kiss", TrackDuration:"5:45"},
+    { TrackImage:"./images/dua_lipa.jpg", TrackName:"Hotter Than Hell", TrackArtist:"Dua Lipa", TrackAlbum:"Kiss", TrackDuration:"5:45"}
+];
